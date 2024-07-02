9b6a14730dce236bda2faf7ec83f3c7c8cfdc00d
kada <adakennedy6@gmail.com>
2024-07-02 22:49:37 +0300

    added comments to explain the code

--- main.go
+++ main.go
@@ -8,15 +8,22 @@ import (
 )
 
 func main() {
+	// Get command-line arguments
 	args := os.Args[1:]
+
+	// Check if the correct number of arguments is provided
 	if len(args) != 2 {
 		fmt.Println("provide input text and banner name")
 		return
 	}
+
+	// Generate ASCII art
 	artText, err := ascii.ArtStringBuilder(args[0], "./banners/"+args[1]+".txt")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
+	// Print the ASCII art
 	fmt.Print(artText)
 }

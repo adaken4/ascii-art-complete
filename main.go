b767abb97792f66f2638b01119aa47ef8c1c0989
kada <adakennedy6@gmail.com>
2024-07-02 22:38:25 +0300

    implemented functionality to provide banner name from arguments

--- main.go
+++ main.go
@@ -3,37 +3,20 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 
 	"asciiart/ascii"
 )
 
 func main() {
 	args := os.Args[1:]
-	if len(args) != 1 {
-		fmt.Println("provide ascii text as one argument")
+	if len(args) != 2 {
+		fmt.Println("provide input text and banner name")
 		return
 	}
-	artText, err := ArtBuilder(args[0])
+	artText, err := ascii.ArtStringBuilder(args[0], "./banners/"+args[1]+".txt")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	fmt.Print(artText)
-}
-
-func ArtBuilder(s string) (string, error) {
-	asciiMap, err := ascii.RuneAsciiArtMapCreator("thinkertoy.txt")
-	if err != nil {
-		return "", err
-	}
-	var result strings.Builder
-	for i := 0; i < 8; i++ {
-		for _, v := range s {
-			artLines := strings.Split(asciiMap[v], "\n")
-			result.WriteString(artLines[i])
-		}
-		result.WriteString("\n")
-	}
-	return result.String(), nil
 }
